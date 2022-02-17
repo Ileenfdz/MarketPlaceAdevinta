8d703892e2481018e8b7304716a5506e14f3c0f0
Ileenfdz <ileenfdz@gmail.com>
2022-02-17 09:09:46 +0100

    Added variables from figma, created route index, and base components header, footer and card

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <header class="d-flex justify-content-center align-items-center py-3">
+        <a href="{{ route('index')}}" class="d-flex align-items-center me-auto  ms-5 text-decoration-none">
+            <span id="title">Market place</span>
+        </a>
+
+        {{$slot}}
+
+    </header>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::resource()
 
 Auth::routes();
 
